207337fa08c4bb721305802fdc4928d36b38094a
Kayla <kay.laverne.3@gmail.com>
2019-10-26 13:19:03 -0400

    Update taskInput.java

--- taskInput.java
+++ taskInput.java
@@ -1,5 +1,14 @@
 package schedulizer;
 
+import javax.swing.*;
+
 public class taskInput {
-	
+	public static void main(String args[]){
+		JFrame frame = new JFrame("Task");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,300);
+		JButton button = new JButton("Submit");
+		frame.getContentPane().add(button);
+		frame.setVisibible(true);
+	}
 }
